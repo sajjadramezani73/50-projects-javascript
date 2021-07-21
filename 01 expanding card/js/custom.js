06f78731a92a75607798639e795a1512439b0e7c
sajjadramezani73 <sajjad.payam77@gmail.com>
2021-07-21 15:10:02 +0430

    change2

--- 01 expanding card/js/custom.js
+++ 01 expanding card/js/custom.js
@@ -1,11 +1,36 @@
-const divElements = document.querySelectorAll('.box')
+const gallery = document.querySelector('.gallery')
 
-divElements.forEach((element) => {
-    element.addEventListener('click', () => {
-        console.log(`${element.parentElement.classList} = ${element.innerHTML}`);
-    })
+const data = [
+    {
+        id: 1, src: "./images/01(1).jpg",
+    },
+    {
+        id: 2, src: "./images/01(2).jpg",
+    },
+    {
+        id: 3, src: "./images/01(3).jpg",
+    },
+    {
+        id: 4, src: "./images/01(4).jpg",
+    },
+    {
+        id: 5, src: "./images/01(5).jpg",
+    },
+]
+
+const createEl = (item) => {
+    console.log(item);
+    let div = document.createElement('div')
+    let img = document.createElement('img')
+    img.setAttribute('src', `${item.src}`)
+    div.appendChild(img)
+    gallery.appendChild(div)
+}
+
+const result = data.forEach((item) => {
+    createEl(item)
 })
 
-let string = 'sajjad'
+console.log(result);
 
-console.log(string == true);
+// gallery.appendChild(result)
